742a917ab41bb34be4e45dc70266d9f3d35bbe68
Chien Trang Minh <17082958+Chien10@users.noreply.github.com>
2019-04-07 17:02:12 +0700

    Try to complete: get input, check if input's valid, print choices and get request, switch case to perform relevent task. Haven't finished get input adn switch case

--- project.asm
+++ project.asm
@@ -1,11 +1,14 @@
 				.data
 
+choice:			.word 		0
+result:			.word 		0
+
 TIME:			.space		12			# 3 * (4 bytes) <--> three memory words (blocks) to store day, month and year
 
 TIME_1:			.space		12
 TIME_2:			.space		12
 
-new_line:			"\n"
+new_line:		.asciiz		"\n"
 
 input_day: 		.asciiz		"Nhap ngay DAY: "
 input_month: 	.asciiz		"Nhap thang MONTH: "
@@ -21,7 +24,7 @@ sec_choice_b:	.asciiz		"			B. Month DD, YYYY\n"
 sec_choice_c:	.asciiz		"			C. DD Month, YYYY\n"
 
 third_choice:	.asciiz		"3. Cho biet ngay vua nhap la ngay thu may trong tuan.\n"
-sub_output1_third_choice:	.asciiz 	"Ngay vua nhap la ngay thu ";
+sub_output1_third_choice:	.asciiz 	"Ngay vua nhap la ngay thu "
 sub_output2_third_choice:	.asciiz		" trong tuan.\n"
 
 fourth_choice:	.asciiz		"4. Kiem tra nam trong chuoi TIME co phai la nam nhuan khong.\n"
@@ -42,10 +45,88 @@ seventh_choice:	.asciiz		"7. Kiem tra bo du lieu dau vao khi nhap, neu du lieu k
 output_valid_date:			.asciiz		"Bo du lieu nhap vao hop le.\n"
 output_invalid_date:		.asciiz		"Bo du lieu nhap vao khong hop le.\n"
 
-input_choice:	.asciiz		"Lua chon: "
+input_choice:	.asciiz		"			Lua chon: "
+invalid_choice:	.asciiz		"Lua chon khong hop le. Vui long nhap lai.\n"
 
-output_result:	.asciiz		"Ket qua: "
+output_result:	.asciiz		"			Ket qua: "
 
 				.text
+main:	
 
+print_tasks:
+			li $v0, 4
+			la $a0, intro_string
+			syscall
 
+			li $v0, 4
+			la $a0, first_choice
+			syscall			
+
+			li $v0, 4
+			la $a0, second_choice
+			syscall 
+			li $v0, 4
+			la $a0, sec_choice_a
+			syscall
+			li $v0, 4
+			la $a0, sec_choice_b
+			syscall
+			li $v0, 4
+			la $a0, sec_choice_c
+			syscall
+
+			li $v0, 4
+			la $a0, third_choice
+			syscall
+
+			li $v0, 4
+			la $a0, fourth_choice
+			syscall
+
+			li $v0, 4
+			la $a0, fifth_choice
+			syscall
+
+			li $v0, 4
+			la $a0, sixth_choice
+			syscall			
+
+			li $v0, 4
+			la $a0, seventh_choice
+			syscall
+
+get_task:
+			li $v0, 4
+			la $a0, input_choice
+			syscall
+
+			li $v0, 5
+			syscall
+			# Whats about task 2?
+
+			sw $v0, choice 		# Save user's input into choice variable
+
+perform_equivalent_request:
+							# Switch case statement
+							# Reference: Kien_truc_bo_lenh_mips.pdf
+							lw $a1, choice
+
+							ble $a1, $0, report_invalid_input		# Check if choice < 0
+
+							addi, $t0, $0, 7
+							bgt $a1, $t0, report_invalid_input		# Check if choice > 7
+
+							j print_string_in_ddmmyyyy_format
+print_string_in_ddmmyyyy_format:
+								addi $t0, $a1, -1
+								bne $t0, $0, print 
+								# To be continued...
+report_invalid_input:
+						li $v0, 4
+						la $a0, invalid_choice
+						syscall
+						j get_task
+
+exit:
+			li $v0, 10
+			syscall			
